49a4601af027c0a5fdc7613746fd0d10f191b9c6
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-01-11 02:24:10 -0500

    add views and urls

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
-from .views import HomePageView
-
+from .views import (
+    HomePageView,
+    WelcomePageView,
+    ClassroomPageView,
+    SchoolAgePageView,
+    SteamPageView,
+    PottyTrainingPageView,
+    PreschoolPageView,
+    ContactPageView,
+    SubscribePageView,
+)
 urlpatterns = [
+    path("subscribe/", SubscribePageView.as_view(), name="subscribe"),
+    path("welcome/", WelcomePageView.as_view(), name="welcome"),
+    path("classrooms/", ClassroomPageView.as_view(), name="classrooms"),
+    path("steam/", SteamPageView.as_view(), name="steam"),   
+    path("preschool/", PreschoolPageView.as_view(), name="preschool"), 
+    path("school-age/", SchoolAgePageView.as_view(), name="school_age"), 
+    path("potty-training/", PottyTrainingPageView.as_view(), name="potty_training"), 
+    path("contact/", ContactPageView.as_view(), name="contact"), 
     path("", HomePageView.as_view(), name="home"),
-]
+]   
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,27 @@
 from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+class WelcomePageView(TemplateView):
+    template_name = "welcome.html"
+
+class ClassroomPageView(TemplateView):
+    template_name = "classroom.html"
+
+class SteamPageView(TemplateView):
+    template_name = "steam.html"
+
+class PreschoolPageView(TemplateView):
+    template_name = "preschool.html"
+
+class SchoolAgePageView(TemplateView):
+    template_name = "school_age.html"
+
+class PottyTrainingPageView(TemplateView):
+    template_name = "potty_training.html"
+
+class ContactPageView(TemplateView):
+    template_name = "contact.html"
+
+class SubscribePageView(TemplateView):
+    template_name = "subscribe.html"
