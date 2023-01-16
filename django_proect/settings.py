95910f92059a184cf0f136f0c16ce57ad789c26a
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-01-16 10:01:40 -0500

    update environment settings

--- django_proect/settings.py
+++ django_proect/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env() 
+env.read_env() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*fjz-9+*#rzq4mskk-$n-*#(6gy0!h!ab^$nx%n8^bpt6*g8%u'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".vercel.app", ".github.io", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -81,10 +85,7 @@ WSGI_APPLICATION = 'django_proect.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.dj_db_url("DATABASE_URL")
 }
 
 
