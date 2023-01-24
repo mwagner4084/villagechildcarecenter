7940fc17e36ac4247b2ed10f80e91f444fbee3dd
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-01-24 11:24:18 -0500

    add railway to allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["railway.app", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["web-production-b31d.up.railway.app", "railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
