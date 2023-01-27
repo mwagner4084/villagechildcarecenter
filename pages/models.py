5da2e9a7f3917790c383f463bbcb5f2f58307450
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-01-26 19:20:04 -0500

    add html field to model

--- pages/models.py
+++ pages/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+from tinymce.models import HTMLField
+
+class Homepage(models.Models):
+    title = models.CharField(max_length=200)
+    content = HTMLField()
+    #image = models.ImageField()
