a73f9e899d294dcc8b94840f3858c4a659d8155f
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-01-26 19:22:49 -0500

    fix typo

--- pages/models.py
+++ pages/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from tinymce.models import HTMLField
 
-class Homepage(models.Models):
+class Homepage(models.Model):
     title = models.CharField(max_length=200)
     content = HTMLField()
     #image = models.ImageField()
