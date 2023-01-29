4e65a14ebd6b5cfa5f2838324c6d72a60972ebe1
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-01-29 00:11:57 -0500

    update views to extend generic PageView

--- pages/views.py
+++ pages/views.py
@@ -2,38 +2,51 @@ from django.views.generic import TemplateView
 #from django.shortcuts import render
 from .models import Page
 
-class HomePageView(TemplateView):
-    template_name = "index.html"
+class PageView(TemplateView):
     model = Page
+    handle = 'home'
+    template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        context = super(HomePageView, self).get_context_data(**kwargs)
-        pages = Page.objects.filter(handle='home')
+        context = super(TemplateView, self).get_context_data(**kwargs)
+        pages = Page.objects.filter(handle=self.handle)
         if len(pages):
             context['page'] = pages[0]
         return context
-    
 
-class WelcomePageView(TemplateView):
+
+class HomePageView(PageView):
+    template_name = "index.html"
+    handle = 'home'
+
+class WelcomePageView(PageView):
     template_name = "welcome.html"
+    handle = 'philosophy'
 
-class ClassroomPageView(TemplateView):
+class ClassroomPageView(PageView):
     template_name = "classrooms.html"
+    handle = 'classrooms'
 
-class SteamPageView(TemplateView):
+class SteamPageView(PageView):
     template_name = "steam.html"
+    handle = 'steam'
 
-class PreschoolPageView(TemplateView):
+class PreschoolPageView(PageView):
     template_name = "preschool.html"
+    handle = 'preschool'
 
-class SchoolAgePageView(TemplateView):
+class SchoolAgePageView(PageView):
     template_name = "school_age.html"
+    handle = 'school_age'
 
-class PottyTrainingPageView(TemplateView):
+class PottyTrainingPageView(PageView):
     template_name = "potty_training.html"
+    handle = 'potty_training'
 
-class ContactPageView(TemplateView):
+class ContactPageView(PageView):
     template_name = "contact.html"
+    handle = 'contact'
 
-class SubscribePageView(TemplateView):
+class SubscribePageView(PageView):
     template_name = "subscribe.html"
+    handle = 'subscribe'
