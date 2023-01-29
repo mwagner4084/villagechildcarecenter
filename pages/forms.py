87fad3e51a19ab106470961fbbb0e38f56a2dc38
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-01-28 23:31:32 -0500

    add secondary content

--- pages/forms.py
+++ pages/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 class HomePageForm(forms.ModelForm):
     class Meta:
-        fields = ['title', 'handle', 'content', 'image']
+        fields = ['title', 'handle', 'content', 'content_secondary']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3}),
+            'content_secondary': forms.Textarea(attrs={'rows': 3}),
         }
