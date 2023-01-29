7c96c15f8084c310ff0afe6a8a080c953d8ee3f8
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-01-29 00:13:04 -0500

    rm default content for now

--- static/js/tinymce/custom.js
+++ static/js/tinymce/custom.js
@@ -67,338 +67,36 @@ const pageDefaults = {
     },
     philosophy: {
         title: `Welcome to the Village`,
-        content: `
-            <p>
-                The early childhood years are a very special time in your child's development. Great changes occur in this short
-                period of time as children learn to communicate, increase their intellectual awareness and make great physical strides.
-            </p>
-            <p>
-                At the Village Childcare Center, we use the Reggio Emilia approach to learning. This approach is a student-centered
-                curriculum that uses self-directed, experiential learning in relationship-driven environments. This program is based on the
-                principles of respect, responsibility and community learned through exploration, discovery and play. At the core of this
-                philosophy is the assumption that children form their own personality during the early years of development and that they are
-                endowed with "a hundred languages", through which they can express their ideas. The aim of the Reggio approach is to teach
-                children how to use these symbolic languages, such as art, in everyday life. Using this philosophy ensures that no two classrooms
-                will be the same. To implement this concept, our teachers will guide students to work on projects that emphasize the interest of
-                the classroom rather than themes that do not benefit your child.
-            </p>
-            <p>
-                Here at The Village, we choose to focus on each individuals' strengths rather than their weaknesses. Our childcare philosophy is to
-                provide an age-appropriate environment that develops self-esteem, confidence, and love of learning. To create these conditions, we
-                utilize our professionally educated staff and the materials to deliver an exceptional program to your child. Educationally, our goal
-                is to foster a love of learning by integrating play and creativity into our curriculum. We challenge our students by promoting inquiry
-                and encouraging discovery through exploring the world around them. This instills a sense of confidence in their ability to master new
-                situations and tasks using their reasoning skills.
-            </p>
-            <p>
-                You know your child best. We encourage you to contact the school about any questions or concerns you might have.
-                We look forward to becoming a part of your child's community.
-            </p>
-        `,
+        content: ``,
     },
     classrooms: {
-        title: `CLASSROOMS`,
-        content: `
-            <h3>
-            <strong>
-                Newborn - 12 Months
-            </strong>
-            </h3>
-            <p>
-                Here, babies are made to feel safe, secure, and happy.
-                This classroom gives warm, caring teachers an opportunity to
-                bond and develop trust with little ones. Each child has a
-                primary caregiver who will guide your child's development and
-                be your main point of contact.
-                <br>
-                <br>
-                In this classroom, children will enjoy a warm, nurturing classroom
-                environment with stimulating activities and age-appropriate educational
-                toys.
-            </p>
-            <br>
-            <h3>
-                <strong>
-                    12 Months - 24 Months
-                </strong>
-            </h3>
-            <p>
-                These energetic little ones are growing and learning, so we channel their
-                energy into positive learning moments by giving them ample room to move
-                and explore. Here, toddlers receive encouragement and guidance toward the
-                growth of their physical skills and emerging milestones:
-                <br>
-                <ul>
-                    <li>
-                        Stimulating classroom environments
-                    </li>
-                    <li>
-                        Milestone-based curriculum
-                    </li>
-                    <li>
-                        Immersive learning experiences
-                    </li>
-                    <li>
-                        Educational toys and activities
-                    </li>
-                </ul>
-            </p>
-            <p>
-                Here, toddlers develop an understanding of their own feelings and
-                relationship with people like family, friends, and community members;
-                they also increase their ability to interact with others and develop
-                self-awareness.
-            </p>
-            <br>
-            <h3>
-                <strong>
-                    24 Months - 35 Months
-                </strong>
-            </h3>
-            <p>
-                This classroom is committed to potty training. A mobile, energetic
-                2-year-old enjoys the entire world as their playground. We've put
-                together a unique educational approach that focuses their enthusiasm
-                into opportunities for development, growth, and achievement. Here,
-                2-year-olds enjoy exclusive milestone-based curriculum and interactive,
-                tactile learning experiences.
-                <br>
-                Developing and understanding of their own emotions and their relationships
-                with others, like family, friends, and community members, as well as
-                understanding how to interact with others while developing self-awareness.
-            </p>
-        `,
-        contentSecondary: `
-            <h3>
-                <strong>
-                    3 - 4 Year Olds
-                </strong>
-            </h3>
-            <p>
-                As they grow to be independent learners, these children develop the ability
-                to work with others, follow directions, self-regulate, follow routines, and
-                build self-help skills. This program offers:
-                <ul>
-                <li>
-                    Proprietary indicator-based curriculum
-                </li>
-                <li>
-                    Opportunities to increase independence
-                </li>
-                <li>
-                    Collaborative learning environments
-                </li>
-                </ul>
-            </p>
-            <p>
-                We will begin more projects in these classrooms- Children will be able to
-                follow their strengths and imagination to explore the world around them.
-            </p>
-            <br>
-            <h3>
-                <strong>
-                    Optional 4-year-old Preschool Program
-                </strong>
-            </h3>
-            <p>
-                In our preschool program our teachers will introduce interscope curriculum. Interscope
-                curriculum offers additional resources designed to support daily learning. This includes
-                strategies to individualize learning throughout the daily routine, as well as activities for building
-                children's literacy skills and music to support learning. These resources are a first step in
-                creating and engaging the rich learning environment that encourages young children to learn
-                through exploration. Combined with diverse, open-ended materials that reflect children's home,
-                language, and culture to provide an optimal learning environment that truly aligns with active
-                learning.
-                <br>
-                <br>
-                Our 4 year olds will have the opportunity to use the STEAM classroom from 9-12 Monday, Wed, Thursday.
-                During this time, students will follow SCOPE curriculum. This program is also available to the public.
-                The Village Childcare center will go in depth with the following:
-                <ul>
-                <li>
-                    <strong>
-                        Nature and Science:
-                    </strong>
-                    to comprehend the physical and natural world through observation, description, prediction, and data gathering
-                </li>
-                <li>
-                    <strong>
-                        Logic and Reasoning:
-                    </strong>
-                    Practicing sequencing, problem-solving, and critical thinking.
-                </li>
-                </ul>
-            </p>
-        `,
+        title: `Classrooms`,
+        content: ``,
+        contentSecondary: ``,
     },
     steam: {
-        title: `SCIENCE. TECHNOLOGY. ENGINEERING. ART. MATHEMATICS.`,
+        title: `Science. Technology. Engineering. Art. Mathematics.`,
         content: `
             <img src="${django.staticBase + 'img/steam.png'}" />
             <h2>
-                <strong>
-                    STEAM
-                </strong>
-                <strong>
-                    EDUCATION
-                </strong>
+                <strong>STEAM</strong> <strong>EDUCATION</strong>
             </h2>
         `,
-        contentSecondary: `
-            <p>
-                Our S.T.E.A.M. classroom is designed to allow children to use their imagination to explore
-                their creativity. Young children benefit from S.T.E.A.M. learning because they are generally
-                naturally inquisitive and want to explore and make sense of themselves and the environment in
-                which they live.
-                <br>
-                <br>
-                In this classroom we will have professionals come on site weekly to teach children science,
-                art, yoga, and music.
-            </p>
-            <p>
-                <img src="${django.staticUrl + 'img/steam_art.png'}" />
-            </p>
-        `,
+        contentSecondary: ``,
     },
     preschool: {
-        title: `COMING FALL 2023`,
-        content: `
-            <p>
-                Our Preschool Program is an optional program as well as open to the public.
-                In our preschool program our teachers will introduce interscope curriculum.
-                <br>
-                <br>
-                <strong>
-                    Interscope curriculum
-                </strong>
-                offers additional resources designed to support daily learning. Includes
-                strategies to individualize learning throughout the daily routine and
-                activities for building children's literacy skills and music to support
-                learning. These resources are a first step in creating and engaging the rich
-                learning environment that encourages young children to learn through exploration.
-                Combined with diverse, open-ended materials that reflect children's home, language,
-                and culture to provide an optimal learning environment that truly aligns with active
-                learning.
-                <br>
-                <br>
-                Our 4 year olds will have the opportunity to use the STEAM classroom from 9-12 Monday,
-                Wed, Thursday. During this time students will follow SCOPE curriculum. This program is
-                also available to the public. The Village Childcare center will go in depth with the following:
-                <br>
-                <br>
-                <strong>
-                    Nature and Science:
-                </strong>
-                to comprehend the physical and natural world through observation, description, prediction,
-                and data gathering.
-                <br>
-                <br>
-                <strong>
-                    Early Math:
-                </strong>
-                Learning about numbers, patterns, sorting, and ordering; learning to utilize numbers to add,
-                subtract, measure, and graph.
-                <br>
-                <br>
-                <strong>
-                    Logic and Reasoning:
-                </strong>
-                Practicing sequencing, problem-solving, and critical thinking.
-            </p>
-        `,
+        title: `Coming Fall 2023`,
+        content: ``,
     },
     school_age: {
-        title: `SCHOOL AGE`,
-        content: `
-        <h3>
-            Before/After School Care
-        </h3>
-        <p>
-            We know it can be stressful to find care for your children
-            before and after school care. We offer care with Kalamazoo
-            bus transportation. While your children are with us during
-            this time, they will have the opportunity to relax, do homework,
-            play outside, and play in our S.T.E.A.M room classroom.
-        </p>
-        <p>
-            <strong>
-                If there is a bus cancellation, we are unable to bus your child to school.
-            </strong>
-        </p>
-        `,
-        contentSecondary: `
-        <h3>
-            Summer Camp + School Breaks
-        </h3>
-        <p>
-            This camp program is for elementary schoolers who are 5-12 years old. We guarantee
-            your children will love spending their summers at the Village Childcare Center. We
-            offer both onsite and offsite field trips. The spaces and activities are designed
-            to be fun, engaging, and educational.
-            <br>
-            <br>
-            No matter the weather, we will be enjoying the outdoors.
-        </p>
-        <br>
-        <p>
-            <strong>
-                The Village Childcare Center follows KPS school schedule.
-            </strong>
-        </p>
-        `,
+        title: `School Age`,
+        content: ``,
+        contentSecondary: ``,
     },
     contact: {
         title: `Contact Us`,
-        content: `
-        <h3>
-            Schedule a tour
-        </h3>
-        <p class="fs-5">
-            We strongly encourage parents to schedule a tour of the school. This will give you the
-            opportunity to see the classrooms and witness first hand what your child can experience
-            at The Village Childcare Center.
-            <br>
-            <br>
-            If you have any questions, concerns, or wish to schedule a tour, please feel free to contact us.
-        </p>
-        `,
-        contentSecondary: `
-        <h3>
-            Location:
-        </h3>
-        <ul>
-            <li>
-                The Village Childcare Center
-            </li>
-            <li>
-                5320 Holiday Terrace
-            </li>
-            <li>
-                Kalamazoo, MI 49009
-            </li>
-        </ul>
-        <br>
-        <ul>
-            <li>
-                <strong>
-                    Local:
-                </strong>
-                (269) 253-8008
-            </li>
-            <li>
-                <strong>
-                    Fax:
-                </strong>
-                (269)222-2588
-            </li>
-            <li>
-                <strong>
-                    Email:
-                </strong>
-                director@thevillageccc.com
-            </li>
-        </ul>
-        `,
+        content: ``,
+        contentSecondary: ``,
     },
 }
 
