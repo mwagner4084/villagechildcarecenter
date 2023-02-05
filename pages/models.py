ec159c8423e20d5e21d7eb25e935e42a8453fd1b
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-02-05 14:27:51 -0500

    change subscribe template to confirm update .py files

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 class Page(models.Model):
     """ Page model for static pages. """
@@ -11,7 +13,7 @@ class Page(models.Model):
         ('preschool', 'Preschool'),
         ('school_age', 'School Age'),
         ('contact', 'Contact'),
-        ('subscribe', 'Subscribe'),
+        ('confirm', 'Confirmation'),
     )
 
     handle = models.CharField(
@@ -26,3 +28,33 @@ class Page(models.Model):
 
     def __str__(self):
         return self.handle
+
+class InformationRequest(models.Model):
+    """ Information Request Model """
+    name = models.CharField(max_length=254, null=True, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.email}"
+
+    def get_absolute_url(self):
+        return reverse("index", args=(self.pk))
+
+class Contact(models.Model):
+    """ Contact Model """
+    fname = models.CharField(max_length=254, null=True, blank=True)
+    lname = models.CharField(max_length=254, null=True, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=254, null=True, blank=True)
+    children = models.CharField(max_length=500, null=True, blank=True)
+    start_date = models.DateField(auto_now_add=True)
+    comments = models.CharField(max_length=500, null=True, blank=True)
+    referred_by = models.CharField(max_length=254, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.email}"
+
+    def get_absolute_url(self):
+        return reverse("contact", args=(self.pk))
--- pages/urls.py
+++ pages/urls.py
@@ -7,16 +7,18 @@ from .views import (
     SteamPageView,
     PreschoolPageView,
     ContactPageView,
-    SubscribePageView,
+    ConfirmPageView,
 )
+from api.views import submit_contact
 
 urlpatterns = [
-    path("subscribe/", SubscribePageView.as_view(), name="subscribe"),
+    path("confirm/", ConfirmPageView.as_view(), name="confirm"),
     path("welcome/", WelcomePageView.as_view(), name="welcome"),
     path("classrooms/", ClassroomPageView.as_view(), name="classrooms"),
     path("steam/", SteamPageView.as_view(), name="steam"),
     path("preschool/", PreschoolPageView.as_view(), name="preschool"),
     path("school-age/", SchoolAgePageView.as_view(), name="school_age"),
     path("contact/", ContactPageView.as_view(), name="contact"),
+    path("submit-contact/", submit_contact, name="submit_contact"),
     path("", HomePageView.as_view(), name="home"),
 ]
