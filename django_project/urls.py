a4fc9d08d97a060af9e858058d2186a5b9ba5e4b
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-02-07 10:19:14 -0500

    add base_site and actions admin template add site settings to admin

--- django_project/urls.py
+++ django_project/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from api import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from api import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -33,3 +34,6 @@ urlpatterns = [
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+admin.site.site_header = "The VCC Admin"
+admin.site.site_title = "VCC admin site"
+admin.site.index_title = "The Village Childcare Center Admin"
